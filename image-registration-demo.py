be4c2b88103c9517cf338701aa89904e18f633b8
AKA Math <amithjkamath@users.noreply.github.com>
2022-10-03 15:27:55 +0200

    Cleaned up: no reset slider shenanigans.

--- image-registration-demo.py
+++ image-registration-demo.py
@@ -12,29 +12,25 @@ def main_loop():
     st.title("Image Registration Demo")
     st.subheader("This app allows you to play with parameters of the transformation matrix!")
     st.text("Use the sliders in the sidebar to modify the parameters of the transformation matrix")
-    st.text(" and try to make the 'Registered' image to be as close to the original as possible.")
+    st.text(" and try to make the 'Registered' image be as close to the original as possible.")
 
-    a = st.sidebar.slider("a", min_value=0.0, max_value=5.0, value=1.0)
-    b = st.sidebar.slider("b", min_value=-1.0, max_value=1.0, value=0.0)
-    c = st.sidebar.slider("c", min_value=-20.0, max_value=20.0, value=0.0)
-    d = st.sidebar.slider("d", min_value=-1.0, max_value=1.0, value=0.0)
-    e = st.sidebar.slider("e", min_value=0.0, max_value=5.0, value=1.0)
-    f = st.sidebar.slider("f", min_value=-20.0, max_value=20.0, value=0.0)
-    #g = st.sidebar.slider("g", min_value=-1.0, max_value=1.0, value=0.0)
-    #h = st.sidebar.slider("h", min_value=-1.0, max_value=1.0, value=0.0)
-
-    #image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
-    #if not image_file:
-    #    return None
+    a = st.sidebar.number_input("a", key="a", min_value=0.0, max_value=5.0, value=1.0, step=0.25)
+    b = st.sidebar.number_input("b", key="b", min_value=-5.0, max_value=5.0, value=0.0, step=0.25)
+    c = st.sidebar.number_input("c", key="c", min_value=-50.0, max_value=50.0, value=0.0, step=5.0)
+    d = st.sidebar.number_input("d", key="d", min_value=-5.0, max_value=5.0, value=0.0, step=0.25)
+    e = st.sidebar.number_input("e", key="e", min_value=0.0, max_value=5.0, value=1.0, step=0.25)
+    f = st.sidebar.number_input("f", key="f", min_value=-50.0, max_value=50.0, value=0.0, step=5.0)
 
     original_image = Image.open("rawimage.png")
     original_image = np.array(original_image)
 
-    test_tform = np.float32([[1.0, 0.5, 5.0], [0.0, 2.0, -5.0], [0.0, 0.0, 1.0]])
+    test_tform = np.float32([[1.0, 0.5, 20.0], [0.0, 2.0, -20.0], [0.0, 0.0, 1.0]])
     deformed_image = cv2.warpPerspective(original_image, test_tform, (512, 512), flags=cv2.INTER_LINEAR)
     tform = np.float32([[a, b, c], [d, e, f], [0.0, 0.0, 1.0]])
 
     registered_image = cv2.warpPerspective(deformed_image, tform, (512, 512), flags=cv2.INTER_LINEAR)
+
+    st.text("The transformation matrix is: [a, b, c; d, e, f; 0, 0, 1]")
 
     st.text("Original Image vs. Deformed Image")
     st.image([original_image, deformed_image], width=200)
