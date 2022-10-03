1617dc416060a35989a9bc0973a8d1546bbc74be
AKA Math <amithjkamath@users.noreply.github.com>
2022-10-03 14:53:00 +0200

    Initial version for public-mode.

--- image-registration-demo.py
+++ image-registration-demo.py
@@ -8,29 +8,20 @@ import numpy as np
 from PIL import Image
 
 
-def brighten_image(image, amount):
-    img_bright = cv2.convertScaleAbs(image, beta=amount)
-    return img_bright
-
-
-def blur_image(image, amount):
-    blur_img = cv2.GaussianBlur(image, (11, 11), amount)
-    return blur_img
-
-
-def enhance_details(img):
-    hdr = cv2.detailEnhance(img, sigma_s=12, sigma_r=0.15)
-    return hdr
-
-
 def main_loop():
-    st.title("OpenCV Demo App")
-    st.subheader("This app allows you to play with Image filters!")
-    st.text("We use OpenCV and Streamlit for this demo")
+    st.title("Image Registration Demo")
+    st.subheader("This app allows you to play with parameters of the transformation matrix!")
+    st.text("Use the sliders in the sidebar to modify the parameters of the transformation matrix")
+    st.text(" and try to make the 'Registered' image to be as close to the original as possible.")
 
-    blur_rate = st.sidebar.slider("Blurring", min_value=0.5, max_value=3.5)
-    brightness_amount = st.sidebar.slider("Brightness", min_value=-50, max_value=50, value=0)
-    apply_enhancement_filter = st.sidebar.checkbox('Enhance Details')
+    a = st.sidebar.slider("a", min_value=0.0, max_value=5.0, value=1.0)
+    b = st.sidebar.slider("b", min_value=-1.0, max_value=1.0, value=0.0)
+    c = st.sidebar.slider("c", min_value=-20.0, max_value=20.0, value=0.0)
+    d = st.sidebar.slider("d", min_value=-1.0, max_value=1.0, value=0.0)
+    e = st.sidebar.slider("e", min_value=0.0, max_value=5.0, value=1.0)
+    f = st.sidebar.slider("f", min_value=-20.0, max_value=20.0, value=0.0)
+    #g = st.sidebar.slider("g", min_value=-1.0, max_value=1.0, value=0.0)
+    #h = st.sidebar.slider("h", min_value=-1.0, max_value=1.0, value=0.0)
 
     #image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
     #if not image_file:
@@ -39,14 +30,17 @@ def main_loop():
     original_image = Image.open("rawimage.png")
     original_image = np.array(original_image)
 
-    processed_image = blur_image(original_image, blur_rate)
-    processed_image = brighten_image(processed_image, brightness_amount)
+    test_tform = np.float32([[1.0, 0.5, 5.0], [0.0, 2.0, -5.0], [0.0, 0.0, 1.0]])
+    deformed_image = cv2.warpPerspective(original_image, test_tform, (512, 512), flags=cv2.INTER_LINEAR)
+    tform = np.float32([[a, b, c], [d, e, f], [0.0, 0.0, 1.0]])
 
-    if apply_enhancement_filter:
-        processed_image = enhance_details(processed_image)
+    registered_image = cv2.warpPerspective(deformed_image, tform, (512, 512), flags=cv2.INTER_LINEAR)
 
-    st.text("Original Image vs Processed Image")
-    st.image([original_image, processed_image])
+    st.text("Original Image vs. Deformed Image")
+    st.image([original_image, deformed_image], width=200)
+
+    st.text("Original Image vs. Registered Image")
+    st.image([original_image, registered_image], width=200)
 
 
 if __name__ == '__main__':
